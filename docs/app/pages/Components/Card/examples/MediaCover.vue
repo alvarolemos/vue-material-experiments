1766668832e315d4101bb83b740a7b185ec61761
Álvaro Lemos <alvarolemos@gmail.com>
2017-10-24 18:56:33 -0200

    docs(MdCard): add new media cover example

--- docs/app/pages/Components/Card/examples/MediaCover.vue
+++ docs/app/pages/Components/Card/examples/MediaCover.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
     <md-card>
+      <md-card-media-cover>
+        <md-card-media md-ratio="16:9">
+          <img src="assets/examples/card-sky.jpg" alt="Skyscraper">
+        </md-card-media>
+
+        <md-card-area>
+          <md-card-header>
+            <span class="md-title">Without text background</span>
+            <span class="md-subhead">I bet you can't read this</span>
+          </md-card-header>
+
+          <md-card-actions>
+            <md-button>Neither this</md-button>
+            <md-button>Or this</md-button>
+          </md-card-actions>
+        </md-card-area>
+      </md-card-media-cover>
+    </md-card>
+
+    <md-card>
       <md-card-media-cover md-text-scrim>
         <md-card-media md-ratio="16:9">
           <img src="assets/examples/card-sky.jpg" alt="Skyscraper">
